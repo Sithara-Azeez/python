f9677114efc697cf86a0ddba389e0d2c00b7a7d8
Sithara Azeez <sitharasithara2@gmail.com>
2022-06-25 17:49:22 +0530

    Python Strings Completed

--- 1.String.py
+++ 1.String.py
@@ -10,3 +10,56 @@ print(a[:11])
 print(a[-8:-1])
 print(a[:-5])
 
+print("-------------------------------------")
+
+#Looping through string
+for x in "sithara":
+    print(x)
+print("-------------------------------------")
+
+#checking a char in a string
+
+txt = "you are pro in Django"
+print("pro" in txt)           # we will use 'in' keyword
+print("-------------------------------------")
+
+#USING IF pro in txt
+if "pro" in txt:
+    print("pro is Present")
+print("-------------------------------------")
+
+#CHECK IF NOT
+print("free" not in txt)           # USING 'not in'
+print("-------------------------------------")
+
+if "expense" not in txt:
+    print("you are right,expense is not there")
+print("-------------------------------------")
+
+''' SLICING STRING''' #using start index and end index
+a = "Hello World"
+print(a[:7])
+print("-------------------------------------")
+
+'''MODIFY STRING''' #Python has a set of built-in-methods
+print(a.upper())
+print(a.lower())
+b = " Hello World "
+print(b.strip())        #remove WHITESPACE
+print(a.replace("H","A"))
+
+print(a.split("W"))     #returns a list with seperation
+print("-------------------------------------")
+
+'''CONCATENATE STRING'''
+a = "hello"
+b = "world"
+c = a + " " + b
+print(c)
+print("-------------------------------------")
+
+'''ESCAPE SEQUENCE'''
+t = "why you called me \"intelligent\" "
+print(t)
+t = 'it\'s alright use \\(backslash)'
+print(t)
