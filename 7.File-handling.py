b2e3a1517fe928bf8d3666010e8d81ac2cc52a13
Sithara Azeez <sitharasithara2@gmail.com>
2022-07-02 02:00:47 +0530

    File-Handling done

---
+++ 7.File-handling.py
@@ -0,0 +1,9 @@
+# write permision
+'''f = open("sithara.py","w")
+f.write("print('hai')")
+f.close()'''
+
+# read permission
+with open("1.Sum of 2 numbers.py","r") as f:
+    x = f.read()
+print(x)
