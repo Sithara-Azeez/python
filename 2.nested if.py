1f3a1c9e1986fc2e5267d890af82737562f9daed
Sithara Azeez <sitharasithara2@gmail.com>
2022-11-23 02:17:37 +0530

    Nested if added

---
+++ 2.nested if.py
@@ -0,0 +1,8 @@
+a = int(input("enter a number"))
+if a >= 0:
+    if a > 0:
+        print("It's a Positive number")
+    else:
+        print("It's a Zero")
+else:
+    print("It's a Negative number")
