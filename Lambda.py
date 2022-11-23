b0f7a1c8771dde8746f8e06e626f9abd05c85456
Sithara Azeez <sitharasithara2@gmail.com>
2022-11-24 02:55:08 +0530

    Lamba added

---
+++ Lambda.py
@@ -0,0 +1,19 @@
+# Lambda
+l = lambda x: x * x
+print(l(10))
+
+print("-------------------------------------")
+
+list1 = [1, 2, 3, 4, 5, 6]
+
+
+# def even(x):
+#     if x % 2 == 0:
+#         return x
+#
+#
+# f = filter(even, list1)
+# print(list(f))
+
+f = filter(lambda x: x % 2 == 0, list1)
+print(list(f))
